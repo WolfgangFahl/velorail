cc753c196022d48a9c86c51917f91f7c65329515
tholzheim <tim.holzheim@rwth-aachen.de>
2025-02-01 16:14:59 +0100

    adds train station lookup by coordinates from Wikidata (closes #3)

---
+++ tests/test_locfind.py
@@ -0,0 +1,34 @@
+import unittest
+
+from velorail.locfind import LocFinder
+
+
+class TestLocFinder(unittest.TestCase):
+    """
+    test locfinder
+    """
+
+
+    def test_get_train_stations(self):
+        """
+        test get_train_stations
+        """
+        locfinder = LocFinder()
+        lod_train_stations = locfinder.get_all_train_stations()
+        print(len(lod_train_stations))
+        self.assertGreaterEqual(len(lod_train_stations), 100000)
+
+    def test_get_nearest_train_station(self):
+        """
+        test get_nearest_train_station
+        """
+        lat = 43.2661645
+        long = -1.9749167
+        distance = 10
+        locfinder = LocFinder()
+        results = locfinder.get_train_stations_by_coordinates(lat, long, distance)
+        print(results)
+        self.assertGreaterEqual(len(results), 30)
+
+if __name__ == '__main__':
+    unittest.main()
